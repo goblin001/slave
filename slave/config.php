e300c22e2c3bb7e34322d79917b51db9c66a26c3
goblin001 <50068770+goblin001@users.noreply.github.com>
2021-06-22 20:10:59 +0800

    Update config.php

--- slave/config.php
+++ slave/config.php
@@ -1,6 +1,6 @@
 <?php
 $nic = 'eth0'; //主网卡名称
-$url = 'https://example.com/modules/addons/PortForward/apicall.php';
+$url = 'https://yinhee.cc/modules/addons/PortForward/apicall.php';
 $key = ''; //在WHMCS设置的key
 $sourceip = ''; //注意，这里并不是外网IP，而是在 ifconfig 中主网卡的IP地址
 $magnification = '1'; //流量倍率 默认为1,双向计算，如需单向计算，输入0.5
